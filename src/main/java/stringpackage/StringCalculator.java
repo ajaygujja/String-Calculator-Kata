7cfc26907b0525bccba3d3da1ca3fc20005bf003
ajaygujja <ajaygujja5@gmail.com>
2020-06-07 00:52:24 +0530

    FIrst Test Case Passed

--- src/main/java/stringpackage/StringCalculator.java
+++ src/main/java/stringpackage/StringCalculator.java
@@ -1,18 +1,11 @@
 package stringpackage;
 
-/**
- * Hello world!
- */
 public final class StringCalculator {
-    private StringCalculator() {
+
+    public int add(String testString) {
+
+        int LengthofString = testString.length();
+        return LengthofString;
     }
 
-    /**
-     * Says hello to the world.
-     *
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
 }
--- src/test/java/stringpackage/StringCalculatorTest.java
+++ src/test/java/stringpackage/StringCalculatorTest.java
@@ -4,15 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
 class StringCalculatorTest {
-    /**
-     * Rigorous Test.
-     */
+
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void testWithSpace() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(0, stringCalculator.add(""));
     }
 }
