145f1127f8523cc1cd0fc705061663c9359251ae
ajaygujja <ajaygujja5@gmail.com>
2020-06-07 00:26:12 +0530

    Initail Commit

---
+++ src/main/java/stringpackage/StringCalculator.java
@@ -0,0 +1,18 @@
+package stringpackage;
+
+/**
+ * Hello world!
+ */
+public final class StringCalculator {
+    private StringCalculator() {
+    }
+
+    /**
+     * Says hello to the world.
+     *
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
