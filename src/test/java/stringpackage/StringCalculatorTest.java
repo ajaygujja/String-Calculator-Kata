947b002ab3215333df7545c9a49a505938eed6df
ajaygujja <ajaygujja5@gmail.com>
2020-06-07 16:36:28 +0530

    Refactor: StringCalculatorTest class

--- src/test/java/stringpackage/StringCalculatorTest.java
+++ src/test/java/stringpackage/StringCalculatorTest.java
@@ -4,24 +4,30 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+
 class StringCalculatorTest {
+
+    static StringCalculator stringCalculator;
+
+    @BeforeEach
+    void initEach() {
+        stringCalculator = new StringCalculator();
+    }
 
     @Test
     void testWithSpace() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
     void testWithOneNumber() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
         assertEquals(5, stringCalculator.add("5"));
     }
 
     @Test
     void testWithTwoNumbers() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(3, stringCalculator.add("1,2"));
         assertEquals(8, stringCalculator.add("5,3"));
         assertEquals(10, stringCalculator.add("5,5"));
