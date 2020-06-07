45460b58c9b97c079220d65710d6f03783aacfee
ajaygujja <ajaygujja5@gmail.com>
2020-06-07 16:23:57 +0530

    Third Test Case Passed

--- src/main/java/stringpackage/StringCalculator.java
+++ src/main/java/stringpackage/StringCalculator.java
@@ -3,13 +3,20 @@ package stringpackage;
 public final class StringCalculator {
 
     public int add(String testString) {
-
+        int count = 0;
         int LengthofString = testString.length();
-        if (LengthofString == 1) {
+
+        if (LengthofString == 0) {
+            return LengthofString;
+        } else if (LengthofString == 1) {
             return Integer.parseInt(testString);
         }
 
-        return LengthofString;
-    }
+        String[] part = testString.split(",");
 
+        for (int i = 0; i < part.length; i++) {
+            count += Integer.parseInt(part[i]);
+        }
+        return count;
+    }
 }
--- src/test/java/stringpackage/StringCalculatorTest.java
+++ src/test/java/stringpackage/StringCalculatorTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringCalculatorTest {
-    
 
     @Test
     void testWithSpace() {
@@ -13,14 +12,18 @@ class StringCalculatorTest {
         assertEquals(0, stringCalculator.add(""));
     }
 
-
-
     @Test
     void testWithOneNumber() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
         assertEquals(5, stringCalculator.add("5"));
     }
+
+    @Test
+    void testWithTwoNumbers() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("1,2"));
+        assertEquals(8, stringCalculator.add("5,3"));
+        assertEquals(10, stringCalculator.add("5,5"));
+    }
 }
-
-
