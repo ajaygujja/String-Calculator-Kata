03a490cd07a5304dad822f3d277fd71204490f1c
ajaygujja <ajaygujja5@gmail.com>
2020-06-07 16:03:35 +0530

    Second Test Case Passed

--- src/main/java/stringpackage/StringCalculator.java
+++ src/main/java/stringpackage/StringCalculator.java
@@ -5,6 +5,10 @@ public final class StringCalculator {
     public int add(String testString) {
 
         int LengthofString = testString.length();
+        if (LengthofString == 1) {
+            return Integer.parseInt(testString);
+        }
+
         return LengthofString;
     }
 
--- src/test/java/stringpackage/StringCalculatorTest.java
+++ src/test/java/stringpackage/StringCalculatorTest.java
@@ -5,10 +5,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringCalculatorTest {
+    
 
     @Test
     void testWithSpace() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
+
+
+
+    @Test
+    void testWithOneNumber() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(1, stringCalculator.add("1"));
+        assertEquals(5, stringCalculator.add("5"));
+    }
 }
+
+
